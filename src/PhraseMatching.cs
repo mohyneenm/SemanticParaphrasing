788d6cbea58f55ec29432819abde1e22e5559240
mohyneenm <mohyneenm@yahoo.com>
2018-08-17 20:31:49 -0400

    Update PhraseMatching.cs

--- src/PhraseMatching.cs
+++ src/PhraseMatching.cs
@@ -1,45 +1,75 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using F23.StringSimilarity;
 using LemmaSharp;
-using Syn.WordNet;
 
-namespace Puzzles
+namespace DayforceAssistant.Infrastructure.NLP
 {
-    class PhraseMatching
+    public class PhraseMatcher
     {
-        public static string[] GetMatchingPhrase(string inputPhrase, IList<string> phrases)
+        /// <summary>
+        ///  Given a list of phrases, return the best matching phrase based on an input phrase. 
+        /// </summary>
+        /// <param name="inputPhrase"></param>
+        /// <param name="phrases"></param>
+        /// <returns></returns>
+        public string[] GetMatchingPhrase(string inputPhrase, List<string> phrases)
         {
-            //var wordNet = new WordNetEngine();
-
             if (phrases?.Count == 0)
                 return null;
 
-            // Remove stop-words
-            inputPhrase = RemoveStopWords(inputPhrase);
-            var lstPhrases = new List<string>();
-            foreach (var phrase in phrases)
+            // Lemmatize and remove stop-words from phrases
+            var lmtz = new LemmatizerPrebuiltCompact(LanguagePrebuilt.English);
+            var lemmaListPhrases = Lemmatize(phrases, lmtz);
+            for(var i=0; i< lemmaListPhrases.Count; i++)
             {
-                lstPhrases.Add(RemoveStopWords(phrase));
+                var phrase = lemmaListPhrases[i];
+                lemmaListPhrases[i] = RemoveStopWords(phrase);
             }
 
-            // Lemmatize
-            var lemmaListPhrases = new List<string[]>();
-            var lmtz = new LemmatizerPrebuiltCompact(LanguagePrebuilt.English);
-            Lemmatize(lstPhrases, lemmaListPhrases, lmtz);
-
-            // Min distance
+            // Lemmatize and remove stop-words from inputPhrase
             var lemmaInputPhrase = string.Join(" ", LemmatizePhrase(lmtz, inputPhrase));
-            var bestMatchIndex = FindMinimumDistance(lemmaInputPhrase, lemmaListPhrases);
+            lemmaInputPhrase = RemoveStopWords(lemmaInputPhrase);
+            lemmaInputPhrase = SubstituteWords(lemmaInputPhrase);   // "your" => "my"
 
-            var paraphrasedResult = Paraphrase(phrases[bestMatchIndex]);
-            return new string[] { phrases[bestMatchIndex], paraphrasedResult };
+            // LCS distance
+            var bestMatchIndex = LongestCommonSubsequence(lemmaInputPhrase, lemmaListPhrases);
+
+            return new string[] { phrases[bestMatchIndex] };
         }
 
-        private static void Lemmatize(List<string> lstPhrases, IList<string[]> lemmaListPhrases, ILemmatizer lmtz)
+        /// <summary>
+        /// Since this algorithm will be used to find best match of a response for a given query, 
+        /// the response string should contain certain transposed matching words, for eg, "my" for "your" 
+        /// </summary>
+        /// <param name="inputPhrase"></param>
+        /// <returns></returns>
+        private string SubstituteWords(string inputPhrase)
         {
+            var pattern = @"\byour\b";
+            var regex = new Regex(pattern);
+            inputPhrase = regex.Replace(inputPhrase, "my");
+
+            pattern = @"\byou\b";
+            regex = new Regex(pattern);
+            inputPhrase = regex.Replace(inputPhrase, "i");
+
+            return inputPhrase;
+        }
+
+        /// <summary>
+        /// Lemmatize the phrases.
+        /// </summary>
+        /// <param name="lstPhrases"></param>
+        /// <param name="lmtz"></param>
+        /// <returns></returns>
+        private List<string> Lemmatize(List<string> lstPhrases, ILemmatizer lmtz)
+        {
+            var lemmaListPhrases = new List<string[]>();
+
             foreach (var phrase in lstPhrases)
             {
                 var words = phrase.Split(
@@ -54,61 +84,32 @@ namespace Puzzles
                     arrOfWords[i] = LemmatizeWord(lmtz, arrOfWords[i]);
                 }
             }
+
+            return lemmaListPhrases.Select(arr => string.Join(" ", arr)).ToList();
         }
 
         /// <summary>
-        /// This function uses Google Cloud Translate to paraphrase an english sentence,
-        /// first by translating it into 2 foreign languages and then back to english.
-        /// Normally a seq2seq text generator should be used here.
+        /// Finds the maximum LCS of an input string against a list of strings.
         /// </summary>
-        /// <param name="input"></param>
+        /// <param name="lemmaInputPhrase"></param>
+        /// <param name="lemmaListPhrases"></param>
         /// <returns></returns>
-        private static string Paraphrase(string input)
+        private int LongestCommonSubsequence(string lemmaInputPhrase, IList<string> lemmaListPhrases)
         {
-            var rnd = new Random();
-            var languages = new List<string> { "fr", "de", "es" };
-            var index = rnd.Next(0, 3);
-
-            // 1st translation to a foreign language
-            var str = Translation.Translate(input, languages[index]);
-
-            // fisher-yates
-            var lastPicked = languages[index];
-            languages[index] = "es";
-            languages[2] = lastPicked;
-
-            // 2nd translation to another foreign language
-            index = rnd.Next(0, 2);
-            str = Translation.Translate(str, languages[index]);
-
-            // final translation back to english
-            str = Translation.Translate(str, "en");
-
-            return str;
-        }
-
-        private static void LoadWordnet(WordNetEngine wordNet)
-        {
-            var directory = Path.Combine(Directory.GetCurrentDirectory(), "wordnet");
-            wordNet.LoadFromDirectory(directory);
-        }
-
-        private static int FindMinimumDistance(string lemmaInputPhrase, IList<string[]> lemmaListPhrases)
-        {
-            var minDistance = int.MaxValue;
+            var maxLCS = 0.0;
             int bestMatchIndex = 0;
             var lemmaInputPhraseOrdered = string.Join(" ", lemmaInputPhrase.Split(new char[] { ' ' }).OrderBy(x => x).ToList());
-         
+
             for (var i = 0; i < lemmaListPhrases.Count; i++)
             {
-                // order the list first before joining into a single string 'cos Levenshtein depends on the order or characters
-                var lemmaListPhrasesOrdered = lemmaListPhrases[i].OrderBy(x => x).ToList();
+                // order the list first before joining into a single string 'cos LCS depends on the order of characters
+                var lemmaListPhrasesOrdered = lemmaListPhrases[i].Split(" ").OrderBy(x => x).ToList();
 
                 var lemmaDicPhrase = string.Join(" ", lemmaListPhrasesOrdered);
-                var dist = EditDistance(lemmaInputPhraseOrdered, lemmaDicPhrase);
-                if (dist < minDistance)
+                var currentLCS = LCS(lemmaInputPhraseOrdered, lemmaDicPhrase);
+                if (currentLCS > maxLCS)
                 {
-                    minDistance = dist;
+                    maxLCS = currentLCS;
                     bestMatchIndex = i;
                 }
             }
@@ -116,26 +117,42 @@ namespace Puzzles
             return bestMatchIndex;
         }
 
-        private static string RemoveStopWords(string inputPhrase)
+        /// <summary>
+        /// Remove stop-words such as "what", "from", "to", etc to increase the probability of matches.
+        /// </summary>
+        /// <param name="inputPhrase"></param>
+        /// <returns></returns>
+        private string RemoveStopWords(string inputPhrase)
         {
-            var stopWords = new[] { "yes", "no", "you", "are", "on", "at", "with", "from", "to", "am", "is", "my", "I", "for", "a", "of", "any" };
+            var stopWords = new[] { "yes", "no", "are", "on", "at", "with", "from", "to", "am", "is", "for", "a", "of", "any", "it", "what", "when", "who", "where", "be" };
             var result = string.Join(" ", inputPhrase.Split(' ').Where(wrd => !stopWords.Contains(wrd)));
 
             return result;
         }
 
-        private static int EditDistance(string s1, string s2)
+        /// <summary>
+        /// Returns the length of the Longest Common Subsequence between two strings
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns></returns>
+        private double LCS(string s1, string s2)
         {
-            var lev = new Levenshtein();
-            return (int)lev.Distance(s1, s2);
+            //var lev = new Levenshtein();
+            //var jw = new JaroWinkler();
+            var lcs = new LongestCommonSubsequence();
+            var len = lcs.Length(s1, s2);
+
+            //return lcs.Distance(s1, s2);
+            return len;
         }
 
-        private static string[] LemmatizePhrase(ILemmatizer lmtz, string phrase)
+        private string[] LemmatizePhrase(ILemmatizer lmtz, string phrase)
         {
             var words = phrase.Split(
                 new char[] { ' ', ',', '.', ')', '(' }, StringSplitOptions.RemoveEmptyEntries);
 
-            for(var i=0; i<words.Length; i++)
+            for (var i = 0; i < words.Length; i++)
             {
                 words[i] = LemmatizeWord(lmtz, words[i]);
             }
@@ -143,7 +160,7 @@ namespace Puzzles
             return words;
         }
 
-        private static string LemmatizeWord(ILemmatizer lmtz, string word)
+        private string LemmatizeWord(ILemmatizer lmtz, string word)
         {
             string wordLower = word.ToLower();
             string lemma = lmtz.Lemmatize(wordLower);
